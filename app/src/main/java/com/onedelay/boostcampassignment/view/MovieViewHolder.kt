467e1092a2394b899069ffb5feedcbbd9743e249
Won ji yeon <wjy0218@gmail.com>
2019-08-23 17:38:06 +0900

    New: Fabric 설정

--- app/src/main/java/com/onedelay/boostcampassignment/view/MovieViewHolder.kt
+++ app/src/main/java/com/onedelay/boostcampassignment/view/MovieViewHolder.kt
@@ -4,12 +4,12 @@ import android.support.v7.widget.RecyclerView
 import com.onedelay.boostcampassignment.databinding.ViewholderItemBinding
 import com.onedelay.boostcampassignment.model.MovieItem
 
-class MovieViewHolder(
-        private val binding: ViewholderItemBinding
-) : RecyclerView.ViewHolder(binding.root) {
+
+class MovieViewHolder(private val binding: ViewholderItemBinding) : RecyclerView.ViewHolder(binding.root) {
 
     fun bind(item: MovieItem) {
         binding.item = item
         binding.executePendingBindings()
     }
+
 }
